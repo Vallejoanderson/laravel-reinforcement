bfe29ddfbb0c14a64ea01ce68d00221e16721857
Anderson Vallejo <vallejoanderson@gmail.com>
2023-04-06 20:16:44 -0500

    feat: create list of post from files a specific directory

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -4,30 +4,9 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-    <article>
-        <h1><a href='/posts/my-first-post'>My First Post</a></h1>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, quas provident recusandae unde explicabo natus laborum quae. Veniam officiis harum rem a maxime illum, alias, magni voluptas sint quam accusamus.
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, quas provident recusandae unde explicabo natus laborum quae. Veniam officiis harum rem a maxime illum, alias, magni voluptas sint quam accusamus.
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, quas provident recusandae unde explicabo natus laborum quae. Veniam officiis harum rem a maxime illum, alias, magni voluptas sint quam accusamus.
-        </p>
-    </article>
-
-    <article>
-        <h1><a href='/posts/my-second-post'>My Second Post</a></h1>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, quas provident recusandae unde explicabo natus laborum quae. Veniam officiis harum rem a maxime illum, alias, magni voluptas sint quam accusamus.
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, quas provident recusandae unde explicabo natus laborum quae. Veniam officiis harum rem a maxime illum, alias, magni voluptas sint quam accusamus.
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, quas provident recusandae unde explicabo natus laborum quae. Veniam officiis harum rem a maxime illum, alias, magni voluptas sint quam accusamus.
-        </p>
-    </article>
-
-    <article>
-        <h1><a href='/posts/my-second-post'>My Third Post</a></h1>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, quas provident recusandae unde explicabo natus laborum quae. Veniam officiis harum rem a maxime illum, alias, magni voluptas sint quam accusamus.
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, quas provident recusandae unde explicabo natus laborum quae. Veniam officiis harum rem a maxime illum, alias, magni voluptas sint quam accusamus.
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, quas provident recusandae unde explicabo natus laborum quae. Veniam officiis harum rem a maxime illum, alias, magni voluptas sint quam accusamus.
-        </p>
-    </article>
+    <?php foreach ($posts as $post): ?>
+        <article>
+            <?= $post; ?>
+        </article>
+    <?php endforeach; ?>
 </body>
